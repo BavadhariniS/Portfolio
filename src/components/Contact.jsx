f0309a38a3c059f3c68a5b3b3126fac472f27e83
BavadhariniS <bavadharini139@gmail.com>
2026-08-04 22:42:53 +0530

    updated linkedin

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ function Contact(){
 
       <p>Email: bavadharini139@email.com</p>
       <p>Phone: +91 8939617997</p>
-      <p>LinkedIn :<a href="https://www.linkedin.com/in/bavadharini-s/ "></a> https://www.linkedin.com/in/bavadharini-s/ </p>
+      <p id="LI">LinkedIn :<a href="https://www.linkedin.com/in/bavadharini-s/ "></a> https://www.linkedin.com/in/bavadharini-s/ </p>
     </section>
   )
 }
